adb91c8c13145dd33a9b76cb98943bb675eb5a9c
Vikas Pal <rohitprotobit@gmail.com>
2020-02-02 17:00:11 +0530

    Recursion problem added

---
+++ RecursionAndBacktracking/fast_power.cpp
@@ -0,0 +1,48 @@
+// Recursion and Backtracking solved problems discussion in the video section 
+
+#include <iostream>
+using namespace std;
+
+int fastPower(int a, int b){
+	if (b==1)
+	{
+		return a;
+	}
+	return a * fastPower(a, b-1);
+}
+
+int  decreaseSequence(int n){
+	if(n==1){
+		return 1;
+	}
+	cout << n ;
+    return decreaseSequence(n-1);
+}
+
+int increaseSequence(int n) {
+	if(n==1){
+		return 1;
+	}
+    increaseSequence(n-1);
+	return n ;
+}
+// Mutliply Operation without using the * operator
+
+int multiply(int a, int b){
+	if(b==1){
+		return a;
+	}
+	return a + multiply(a,b-1);
+
+}
+
+int main(int argc, char const *argv[])
+{
+	/*// cout << fastPower(2,6);
+	cout << decreaseSequence(5)<<endl;	
+	cout << increaseSequence(5) <<endl;*/
+
+	cout << multiply(2,6) << '\n';
+
+	return 0;
+}
