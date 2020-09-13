cb07dc75f8e20c79b6d325055bf532c286f558c5
palrohitg <rohitprotobit@gmail.com>
2020-09-13 23:38:09 +0530

    optimal_game_stratery

--- 40 L40 Dynamic Programing Challenges/optimal_game_stratery.cpp
+++ 40 L40 Dynamic Programing Challenges/optimal_game_stratery.cpp
@@ -1,19 +1,32 @@
+// Choose the optima l moves in each steps 
+// time complexity and overlapping subproblems 
+
+
 #include<iostream> 
+#include<cstring> 
 #include<climits>
 using namespace std; 
+
+int dp[5000][5000]; 
 
 int optimal(int arr[], int i, int j) {
 
     // Base Case 
-    
+    if (j == i + 1) {
+        return max(arr[i], arr[j]); 
+    }
 
-    // Recursive Case 
-    int ans = INT_MIN; 
-    int opt1 = arr[i] + min(optimal(arr, i + 2, j), optimal(arr, i + 1, j - 1));
+    if (dp[i][j] != -1) {
+        return dp[i][j];
+    }
+
+    // Choose diagrams if i choose the left and right we going to have to two results for sure we need to choose the best 
+    int ans = INT_MIN;
+    int opt1 = arr[i] + min(optimal(arr, i + 2, j), optimal(arr, i + 1, j -1));
     int opt2 = arr[j] + min(optimal(arr, i + 1, j - 1), optimal(arr, i, j - 2));
-    ans = max(opt1, opt2); 
-    
-    return ans; 
+    ans = max(opt1, opt2);
+
+    return dp[i][j] = ans; 
 }
 
 
@@ -26,6 +39,7 @@ int main() {
     for(int i = 0; i < n; i++) {
         cin >> arr[i];
     }
+    memset(dp, -1, sizeof(dp));
 
     cout << optimal(arr, 0, n - 1);
 
