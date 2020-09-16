26c900138c7ae21e3a434ae3504b82092bd54eaf
palrohitg <rohitprotobit@gmail.com>
2020-09-16 23:43:45 +0530

    expediation_greedy_problems_added

---
+++ 38 L38 Challenges Greedy Algorihtm/bailife.cpp
@@ -0,0 +1,59 @@
+/*
+    1. No of jobs transfer between one part to another parts 
+    2. Running Differences make
+
+    Make the balances arrays if we can transfer the jobs to neigbour processors 
+
+*/
+
+#include<iostream>
+#include<climits>
+using namespace std; 
+
+int main() {
+
+    int n; cin >> n;
+    int a[100];
+    int i;
+
+    int sum = 0;
+
+    for(int i = 0; i < n; i++) {
+       cin >> a[i];
+       sum += a[i]; 
+    }
+
+    if(sum % n != 0) { 
+        // can't make the balances arrays  
+        cout << "-1"; 
+    }
+    else {
+        
+int count=0;
+
+int average=sum/n;
+
+for(i=0; i < n-1;i++)
+
+{
+
+int diff=a[i]-average;
+
+a[i+1]+=diff;
+
+count =max(count,abs(a[i]-average));
+
+}
+
+count =max(count,abs(a[i]-average));
+
+cout<<count<<endl; 
+    }
+
+
+
+
+
+    return 0; 
+}
+
---
+++ 38 L38 Challenges Greedy Algorihtm/exepdi.cpp
@@ -0,0 +1,15 @@
+/* 
+
+1. Need to keeps track of the largest fuel which are aviable in the paths means from where i can travels. 
+// Find the largest elements in the running streams so the priority Queues Data strucuture we are going to use 
+
+*/
+#include<iostream>
+using namespace std;
+
+int main() {
+
+
+
+    return 0;
+}
