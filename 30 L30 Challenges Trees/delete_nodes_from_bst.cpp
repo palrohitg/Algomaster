3937fc3d67708a74655230145038f8d4677bddec
palrohitg <rohitprotobit@gmail.com>
2020-09-24 23:48:33 +0530

    graphs_traversal

---
+++ 30 L30 Challenges Trees/delete_nodes_from_bst.cpp
@@ -0,0 +1,26 @@
+#include<bits/stdc++.h> 
+using namespace std;
+
+class node {
+    
+    public : 
+        int data; 
+        node *left; 
+        node *right; 
+
+        node (int data) {
+            this->data = data;
+            left = NULL;
+            right = NULL; 
+        }
+}; 
+
+
+
+
+
+int main() {
+
+
+    return 0;
+}
