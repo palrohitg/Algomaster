e177468e5deeed8d082493bb42643b956794fca3
palrohitg <rohitprotobit@gmail.com>
2020-10-06 23:49:55 +0530

    pairing problem

---
+++ 41 L41 Graph Algorithm/DSU.cpp
@@ -0,0 +1,112 @@
+/*
+Disjoint Set union Data structure
+------------------ -----------------
+Operation : 
+1. Union => Combine two different set  
+2. find  => find the set in which the elements belongs to
+
+
+
+Optimization 
+-------------------------------------
+1. Path compression algorithms 
+2. Order by rank (maintain the number of elements present in the set or ranks)
+------------------------------------
+
+Application : 
+1. cycle detections  
+2. 
+
+*/ 
+
+#include<bits/stdc++.h> 
+using namespace std; 
+
+
+class DSU {
+
+    int V: 
+    list<pair<int, int> > l;
+
+    public : 
+
+        DSU(int V) {
+            this->V = V; 
+        }
+
+        void addEdge(int a, int b) {
+            l.push_back(make_pair(a, b));
+        }
+        // 1. find operations 
+
+        // int findSet(int i, int *parent) {
+        //     if (parent[i] == -1) {
+        //         return i; 
+        //     }        
+        //     return parent[i] = findSet(parent[i]);
+        // }
+
+
+
+
+        // // 2. Union Operation; 
+
+
+        // void union_set(int a, int b, int parent[]) {
+            
+        //     int s1 = findSet(a, parent); // O(N) in worst case searching 
+        //     int s2 = findSet(b, parent); 
+            
+        //     // belongs to different set merge the set 
+        //     if (s1 != s2) {
+        //         parent[s2] = s1;  
+        //     }
+        // }
+
+        // bool detect_cycle() {
+        //     int *parent = new int[V];
+
+        //     for (int i = 0; i < V; i++) {
+        //         parent[i] = -1;
+        //     }
+
+        //     // Iterate over the edgelist 
+        //     for (auto edge : l) {
+                
+        //         int i = edge.first; 
+        //         int j = edge.second; 
+
+        //         int s1 = findSet(i, parent); 
+        //         int s2 = findSet(j, parent); 
+
+        //         if(s1 != s2) {
+        //             union_set(i, j, parent); 
+        //         }
+        //         else {
+        //             return true;
+        //         }
+        //     }
+        //     delete [] parent;
+        //     return false;
+        // }
+};
+
+
+
+
+int main() {
+    
+    DSU l(4); 
+    l.addEdge(1,2);
+    l.addEdge(2,3);
+    l.addEdge(3,4);
+    l.addEdge(4,1);
+
+    return 0;
+}
+
+
+
+
+
+
---
+++ 41 L41 Graph Algorithm/pairing_problem.cpp
@@ -0,0 +1,108 @@
+// Every pair of city is consider twice because here order doesn't matters 
+// BFS ans DFS can be used to count the no of ways 
+
+#include<bits/stdc++.h> 
+using namespace std; 
+
+class DSU {
+
+    int V; 
+    list<pair<int, int> > l;     
+
+    public : 
+
+        DSU(int V) {
+            this->V = V; 
+        }
+
+        void addEdge(int a, int b) {
+            l.push_back(make_pair(a, b)); 
+        }
+
+        
+
+        // 1. find 
+        int findSet(int i, int *parent) {
+
+            // Base Case 
+            if (parent[i] == -1) {
+                return i; 
+            }
+            
+            // path compreshion 
+            return parent[i] = findSet(parent[i], parent);
+        } 
+        
+
+
+        void union_set(int x, int y,int *parent, int *rank) {
+
+            int s1 = findSet(x, parent);
+            int s2 = findSet(y, parent);
+
+            // If not same the union the sets 
+            if (s1 != s2) {
+
+                if (rank[s1] < rank[s2]) {
+                    parent[s1] = s2;
+                    rank[s2] += rank[s1];
+                }
+                else {
+                    parent[s2] = s1;
+                    rank[s2] += rank[s1];
+                }
+            }  
+        }
+
+        void pairing() {
+
+            int *parent = new int[V]; 
+            int *rank = new int[V]; 
+
+            // Assign the initial to parent and rank arrays 
+            for (int i = 0; i < V; i++) {
+                parent[i] = -1; 
+                rank[i] = 1; 
+            } 
+            
+            // Iterate over the edge list 
+            for (auto p : l) {
+                
+                // making the elments 
+                int a = p.first; 
+                int b = p.second; 
+                
+                // int s1 = findSet(a, parent); 
+                // int s2 = findSet(b, parent);
+
+                union_set(a, b, parent, rank);
+            } 
+
+            // Compute the no of paths so that there is not any paths 
+            int ans = 0; 
+            for (int i = 0; i < V; i++) {
+                ans += V - rank[findSet(i, parent)];
+            }
+
+            cout << ans/2 << endl;
+        }
+};
+
+
+
+
+int main() {
+
+    int n,m; cin >> n >> m; 
+    DSU g(n); 
+
+    for (int i = 0; i < m; i++) {
+        int x, y; cin >> x >> y; 
+        g.addEdge(x, y);
+    }
+
+    g.pairing();
+
+
+    return 0;
+}
