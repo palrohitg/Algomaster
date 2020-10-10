fd8c24e12da1ef6b3a6cd9a9def6d9023cfb3441
palrohitg <rohitprotobit@gmail.com>
2020-10-10 23:14:52 +0530

    bellmanford

--- 41 L41 Graph Algorithm/krushkal.cpp
+++ 41 L41 Graph Algorithm/krushkal.cpp
@@ -1,117 +1,99 @@
-// implements the same algorithms using prims's MST
-
-
 #include<bits/stdc++.h> 
-using namespace std; 
+using namespace std;
 
+// implement the DSU data structure
+// 1. Path compression 
+// 2. Union by ranks 
 
-// Disjoint set union Data structures 
-class DSU {
+class DSU{
 
     int *parent; 
     int *rank; 
-    int n; 
 
     public : 
 
-
+        // Initialize of rank and parent arrays 
         DSU(int n) {
 
-            this->n = n;
-
-            parent = new int[n];
+            // create the arrays
+            parent = new int[n]; 
             rank = new int[n];
 
-            for (int i = 0; i < n; i++) {
-                parent[i] = -1;
-                rank[i] = 1;
-            }
-        }            
-        // find set function using path compression 
-        int findSet(int i) {
 
-            // Base Case 
-            if(parent[i] == -1) {
-                return i; 
-            } 
-            
-            // path compression 
-            return parent[i] = findSet(parent[i]);
+            for(int i = 0; i < n; i++) {
+                parent[i] = -1; 
+                rank[i] = 1; 
+            }
         }
 
-        // union 
+        int findSet(int i) {
+            // BC 
+            if (parent[i] == -1) {
+                return i; 
+            }
+            return parent[i] = findSet(parent[i]); 
+        }
+
+        // Union functions 
         void unite(int x, int y) {
-            
+
             int s1 = findSet(x); 
             int s2 = findSet(y); 
 
             if (s1 != s2) {
                 if(rank[s1] < rank[s2]) {
                     parent[s1] = s2; 
-                    rank[s2] += rank[s1]; 
+                    rank[s2] += rank[s1];
                 }
                 else { 
-                    parent[s2] = s1; 
+                     parent[s2] = s1; 
                     rank[s1] += rank[s2];
                 }
             }
         }
-
-
 };
 
-class Graph {
+// Graph Class 
+class Graph{
 
-    vector<vector<int>> edgeList; 
-    int V; // no of vertices we have 
-
+    int V; // vertex 
+    vector<vector<int>> v;
     public : 
-
         Graph(int V) {
-            this->V = V;
+            this->V = V; 
         }
 
-        void addEdge(int x, int y, int w) {
-            edgeList.push_back({w, x, y});
-        }   
+        // add edge and weight to the vectors 
+        void addEdge(int s, int d, int w) {
+            v.push_back({w, s, d});
+        }
 
-        int kruskal_mst() {
+        void kruskal() {
+
+            // sort the vector according to vectors
+            sort(v.begin(), v.end());
             
-            // Sort the edgelist based upon their weights 
-            sort(edgeList.begin(), edgeList.end());
-
-            DSU s(V); // Creating the object of one class to another class 
+            // create the dsu object 
+            DSU s(V); 
             int ans = 0; 
+            for(auto x: v) {
+               
+                int w = x[0];
+                int src = x[1];
+                int dest = x[2];
 
-
-            for (auto e : edgeList) {
-                
-                int w = e[0];
-                int x = e[1];
-                
-                int y = e[2]; 
-
-                // if belongs to different components then unite them and add the weights to the ans 
-                if(s.findSet(x) != s.findSet(y)) {
+                // if not contain the cycle than add the addege 
+                if (s.findSet(src) != s.findSet(dest)) {
+                    s.unite(src, dest);
                     ans += w; 
                 }
+                // leave it; 
             }
-            return ans; 
+
+            cout << ans; 
         }
-};
+}; 
 
 
-int main() {
 
-    int V,E; cin >> V >> E; 
-    Graph g(V); 
 
-    for (int i = 0; i < E; i++) {
-        int x, y, z; cin >> x >> y >> z; 
-        g.addEdge(x, y, z);
-    }
-
-    cout << g.kruskal_mst();
-
-    return 0;
-}
--- 41 L41 Graph Algorithm/prims.cpp
+++ 41 L41 Graph Algorithm/prims.cpp
@@ -1,9 +1,9 @@
-#include<iostream>
+// BFS  + minheaps 
+#include<bits/stdc++.h> 
 using namespace std; 
 
 int main() {
 
 
-
-    
+    cout << "implementation of the prim's algorithms "
 }
