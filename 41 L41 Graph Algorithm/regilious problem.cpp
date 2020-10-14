649ea222299fbd23d0549ce90a6d8a4419315467
palrohitg <rohitprotobit@gmail.com>
2020-10-14 23:51:00 +0530

    new commit

--- 41 L41 Graph Algorithm/regilious problem.cpp
+++ 41 L41 Graph Algorithm/regilious problem.cpp
@@ -2,3 +2,12 @@
 // long challengs 5 questions 
 // leetcode easy problem/ medium 30 minutes
 // codeforces div 2 at short contests 
+
+
+
+
+
+// Start solving problems on the hackeranks and then gradually moves to leetcode and codechefs 
+// Solve the question on the tagwise then solve the problems using the mixtures of the question 
+
+// preparing for sessionals
