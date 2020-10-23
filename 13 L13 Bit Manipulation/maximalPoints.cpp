d0cf479a5c76bc1cc73a8b5c119aa9e2f63898cc
palrohitg <rohitprotobit@gmail.com>
2020-10-23 20:35:23 +0530

    maximalPoints

--- 13 L13 Bit Manipulation/maximalPoints.cpp
+++ 13 L13 Bit Manipulation/maximalPoints.cpp
@@ -2,21 +2,41 @@
 using namespace std; 
 
 
-int maximizingXor(int l, int r) {
+// int maximizingXor(int l, int r) {
 
-    // iterate over all the combinations and find the maximal
-    int ans = 0;      
-    for (int i = l; i <= r; i++) {
-        for (int j=l; j <= r; i++) {
-            int cur = i ^ j; 
-            if (cur > ans) {
-                ans = cur; 
+//     // iterate over all the combinations and find the maximal
+//     int ans = 0;      
+//     for (int i = l; i <= r; i++) {
+//         for (int j=l; j <= r; i++) {
+//             int cur = i ^ j; 
+//             if (cur > ans) {
+//                 ans = cur; 
+//             }
+//         }
+//     }  
+
+//     return ans; 
+// }
+
+// if two number have same than max is 0 and to one iteratations 
+int maximizingXor(int l, int r) {
+    int max = 0; 
+    if (l == r) {
+        max = 0; 
+    }
+    else { 
+        // iterate over the ranges and update the max xor 
+        for (int i = l; i <= r; i++) {
+            int res = l ^ i; 
+            if ( res > max) {
+                max = res; 
             }
         }
-    }  
-
-    return ans; 
+    }
+    return max; 
 }
+
+
 
 int main() {
     
---
+++ 13 L13 Bit Manipulation/test.cpp
@@ -0,0 +1,18 @@
+#include<iostream> 
+using namespace std; 
+
+int main() {
+
+
+    int arr[1000]; 
+    int n; cin >> n; 
+
+    for (int i = 0; i < n; i++) {
+        int data; cin >> data; 
+        arr[i] = data;
+    }
+
+    
+
+    return 0; 
+}
