80065e1f66bfdeb77165ed18c8924d4101c875f8
palrohitg <rohitprotobit@gmail.com>
2020-11-08 23:53:46 +0530

    submatxi sum

--- 02 L2 Arrays/algo++ Arrays challenges/arrayIntro.cpp
+++ 02 L2 Arrays/algo++ Arrays challenges/arrayIntro.cpp
@@ -1,19 +1,15 @@
-/*
-  All the elements are initialize to zeroes all the another will take the garbage mistake on the better to have to zeroes on the array
-*/
+class Solution {
+public:
+	int maxSubArray(vector<int>& nums) {
 
+		int n = nums.size();
+		int curMax = nums[0];
+		int globalMax = nums[0];
 
-
-#include<iostream>
-using namespace std; 
-
-int main() {
-	int arr[10];
-
-	for (int i = 0; i <10 ; ++i)
-	{
-		cout << arr[i] << " ,";
+		for (int i = 1; i < n; i++) {
+			curMax = max(nums[i], curMax + nums[i]);
+			globalMax = max(curMax, globalMax);
+		}
+		return globalMax;
 	}
-
-	return 0;
-}
+};
