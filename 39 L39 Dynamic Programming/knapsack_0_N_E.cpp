b267de1e9f92e0a55c24c4d902c6eda39bdf2502
palrohitg <rohitprotobit@gmail.com>
2020-12-08 23:45:17 +0530

    knapsack 0-1

---
+++ 39 L39 Dynamic Programming/knapsack_0_N_E.cpp
@@ -0,0 +1,62 @@
+// Include the current elements or leave it
+// Building the solution from bottoms up
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long int
+
+struct item {
+	int wt;
+	int val;
+};
+
+
+ll solve(vector<item> arr, int n, ll W) {
+
+	ll dp[n + 1][W + 1];
+
+	// Initialized the values
+	for (int i = 0; i <= n; i++) {
+		for (int j = 0; j <= W; j++) {
+			dp[i][j] = 0;
+		}
+	}
+
+	/*
+		Iterate over the items and maintain the dp state for the items
+	*/
+	for (int i = 1; i <= n; i++) {
+		for (int j = 1; j <= W; j++) {
+			// cur weight of items
+			int weight = arr[i].wt;
+			if (weight <= j) {
+				dp[i][j] = max(arr[i].val + dp[i - 1][j - arr[i].wt], dp[i - 1][j]);
+			}
+			else {
+				dp[i][j] = dp[i - 1][j];
+			}
+		}
+	}
+	return dp[n][W];
+}
+
+int main() {
+
+// #ifndef ONLINE_JUDGE
+// 	freopen("input.txt", "r", stdin);
+// 	freopen("output.txt", "w", stdout);
+// #endif
+
+	ll n, W; cin >> n >> W;
+	vector<item> arr(n + 1);
+
+	for (int i = 1; i <= n; i++) {
+		cin >> arr[i].wt >> arr[i].val;
+	}
+
+
+	cout << solve(arr, n, W);
+
+
+
+	return 0;
+}
