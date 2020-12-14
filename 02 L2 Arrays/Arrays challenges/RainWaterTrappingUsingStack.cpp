617c53d73ac4466e9c8981ea5e08193a84d7b22c
palrohitg <rohitprotobit@gmail.com>
2020-12-14 23:41:12 +0530

    rain water trap

---
+++ 02 L2 Arrays/Arrays challenges/RainWaterTrappingUsingStack.cpp
@@ -0,0 +1,23 @@
+/*
+
+	Determine how much water you can trap about rain is over.
+
+
+*/
+
+#include<bits/stdc++.h>
+using namespace std;
+
+int main() {
+
+#ifndef ONLINE_JUDGE
+	freopen("input.txt", "r", stdin);
+	freopen("output.txt", "w", stdout);
+#endif
+
+
+
+
+
+	return 0;
+}
