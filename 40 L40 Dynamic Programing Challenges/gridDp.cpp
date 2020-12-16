9e0c77d82e8180bed5bac19a5921c8c4bfdecda8
palrohitg <rohitprotobit@gmail.com>
2020-12-16 23:32:47 +0530

    unique path added

--- 40 L40 Dynamic Programing Challenges/gridDp.cpp
+++ 40 L40 Dynamic Programing Challenges/gridDp.cpp
@@ -1,2 +1,43 @@
 // Grid DP problems
+/*
+	1. min cost path
 
+
+*/
+/*
+
+	Approach 2 :
+	1. Bottom up solutions :
+		a. make the first row and columns as same.
+		b. form 1,1 select the min one in the previous steps
+		c. return the last row and last column index
+*/
+
+
+
+class Solution {
+public:
+	int minPathSum(vector<vector<int>>& grid) {
+
+		int col = grid[0].size();
+		int row = grid.size();
+
+		// make the first row as sames
+		for (int i = 1; i < row; i++) {
+			grid[i][0] += grid[i - 1][0];
+		}
+
+		for (int i = 1; i < col; i++) {
+			grid[0][i] += grid[0][i - 1];
+		}
+
+		// fill the rest of the grids
+		for (int i = 1; i < row; i++) {
+			for (int j = 1; j < col; j++) {
+				grid[i][j] += min(grid[i - 1][j], grid[i][j - 1]);
+			}
+		}
+
+		return grid[row - 1][col - 1];
+	}
+};
---
+++ 40 L40 Dynamic Programing Challenges/uniquePathLeetcode.cpp
@@ -0,0 +1,26 @@
+// Given two numbers find the number of uniques paths exits
+// move down and rights
+
+
+#include<bits/stdc++.h>
+using namespace std;
+
+int main() {
+
+// #ifndef ONLINE_JUDGE
+// 	freopen("input.txt", "r", stdin);
+// 	freopen("output.txt", "w", stdout);
+// #endif
+
+	int arr[10][10] {0};
+
+	for (int i = 0; i < 5; i++) {
+		for (int j = 0; j < 5; j++) {
+			cout << arr[i] << " ";
+		}
+		cout << endl;
+	}
+
+
+	return 0;
+}
