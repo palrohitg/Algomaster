da1bfbb2d93c6c3fd16a276e912744cd14b9a436
palrohitg <rohitprotobit@gmail.com>
2021-01-10 21:37:10 +0530

    file added

--- 22 L22 Linked Lists/test.cpp
+++ 22 L22 Linked Lists/test.cpp
@@ -1,117 +1,31 @@
-#include<iostream>
-using namespace std;
-class node
-{
+/*
+
+	nums = [1, 2, 3, 4, 5, 6, 7] and k = 3
+	rotate 1 steps : [7, 1, 2, 3, 4, 5, 6]
+	rotate 2 steps : [6, 7, 1, 2, 3, 4, 5]
+	rotate 3 steps : [5, 6, 7 ,1, 2, 3, 4]
+
+	Approaches 2 :
+	a. Using a extra arrays (i + k) % length_of_arrays is the orginal index
+	b. Then copies the content of the temp arrays to originals arrays
+
+	Approches 3 : Using Cyclic Replacements
+	lets look laters
+*/
+
+class Solution {
 public:
-    int data;
-    node*next;
-    node(int d)
-    {
-        data = d;
-        next = NULL;
-    }
+	void rotate(vector<int>& nums, int k) {
+
+		vector<int> temp(nums.size());
+
+		for (int i = 0; i < nums.size(); i++) {
+			temp[(i + k) % nums.size()] = nums[i];
+		}
+
+		// Copy the elements again to the original arrays
+		for (int i = 0; i < nums.size(); i++) {
+			nums[i] = temp[i];
+		}
+	}
 };
-void insert_head(node*&head, int data)
-{
-    node*n = new node(data);
-    n->next = head;
-    head = n;
-}
-void insert_last(node*&head, int data)
-{   if (head == NULL)
-    {
-        insert_head(head, data);
-    }
-
-    else {
-        node*temp = head;
-        while (temp->next != NULL)
-        {
-            temp = temp->next;
-        }
-        node*n = new node(data);
-        n->next = temp->next;
-        temp->next = n;
-    }
-}
-void build_l(node*&head, int n)
-{
-    int data;
-
-    int j = 1;
-    while (j <= n)
-    {
-        cin >> data;
-        insert_last(head, data);
-        j += 1;
-    }
-}
-void print_l(node*head)
-{
-    while (head != NULL)
-    {
-        cout << head->data << ",";
-        head = head->next;
-    }
-}
-node* merge_li(node*&head, node*&head1)
-{
-
-    //Base Case
-    if (head == NULL)
-    {
-        return head1;
-    }
-    else if (head1 == NULL)
-    {
-        return head;
-    }
-    node*c;
-    // Rec Case
-    if (head->data < head1->data)
-    {
-        c = head;
-        c->next = merge_li(head->next, head1);
-    }
-    else
-    {
-        c = head1;
-        c->next = merge_li(head, head1->next);
-    }
-    return c;
-}
-void print_li(node*head)
-{
-    while (head != NULL)
-    {
-        cout << head->data << " ";
-        head = head->next;
-    }
-}
-
-int main() {
-
-#ifndef ONLINE_JUDGE
-    freopen("input.txt", "r", stdin);
-    freopen("output.txt", "w", stdout);
-#endif
-    node*head = NULL;
-    node*head1 = NULL;
-    // int t;
-    // cin >> t;
-    // int n1, n2;
-    // for (int i = 0; i < t; i++)
-    // {
-    //     cin >> n1 >> n2;
-    //     build_l(head, n1);
-    //     build_l(head, n2);
-    //     // node*head = merge_li(head, head1);
-    //     // print_li(head);
-
-
-
-    // }
-    build_l(head, 3);
-    print_li(head);
-    return 0;
-}
