b78158d4abcb92b69514080dd94ddf80987fcb1b
Vikas Pal <rohitprotobit@gmail.com>
2021-03-02 22:50:01 +0530

    fang added

---
+++ 02 L2 Arrays/findMajorityElements.cpp
@@ -0,0 +1,21 @@
+/*
+	floor() = largest integer that is smaller than or equal to x (rounds downs the nearest integers)
+
+*/
+
+
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+	// int n; cin >> n;
+	// vector<int> v(n + 1);
+
+	// for (int i = 0; i < n; i++) {
+	// 	int data; cin >> data;
+	// 	v.push_back(data);
+	// }
+	cout << floor(2.1) << endl;
+	return 0;
+}
