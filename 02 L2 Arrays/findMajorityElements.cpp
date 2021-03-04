124aefd0259eac7a1dce4510333886bd07765ebc
Vikas Pal <rohitprotobit@gmail.com>
2021-03-04 23:40:23 +0530

    majority added

--- 02 L2 Arrays/findMajorityElements.cpp
+++ 02 L2 Arrays/findMajorityElements.cpp
@@ -1,12 +1,72 @@
 /*
-	floor() = largest integer that is smaller than or equal to x (rounds downs the nearest integers)
+	Approach 2 : Hashmap
+	Time : O(N)
+	Space : O(N)
+
+	Approach 2 : NlogN Sorting Approach
+	a. n / 2 for the even numbers
+	b. n / 2 + 1 for odd numbers
+
+
+	Approach 3 ; Boyer-Moore Voting Algorithms
+
+
+
+
 
 */
+
+
+
+int VotingAlgo(vector<int> nums) {
+
+	int count = 0;
+	int candidate = 0;
+
+	for (int num : nums) {
+		if (count == 0) {
+			candidate = num;
+		}
+
+		count += (num == candidate) ? 1 : -1;
+	}
+	return candidate;
+}
+
+
 
 
 class Solution {
 public:
 	int majorityElement(vector<int>& nums) {
+
+		unordered_map<int, int> map;
+
+		for (auto num : nums) {
+			map[num]++;
+		}
+
+		int n = nums.size();
+		int temp = n / 2;
+		int ans = 0;
+
+		// iterate over the hashmaps
+		// find the greatest values
+		for (auto p : map) {
+			if (map[p.first] > temp) {
+				ans = p.first;
+				break;
+			}
+		}
+
+		return ans;
+	}
+};
+
+
+class Solution {
+public:
+	vector<int> majorityElement(vector<int>& nums) {
 
 	}
 };
--- 33 L33 Hashing & Hashtable/unordered_maps.cpp
+++ 33 L33 Hashing & Hashtable/unordered_maps.cpp
@@ -1,39 +1,76 @@
 /*
-    Opeartion : 
-    1. insert each query in 0(1) 
-    2. erase 
-    3. find 
+    Opeartion :
+    1. insert each query in 0(1)
+    2. erase
+    3. find
 
-    // value determine the associated contents and key uniquely identify the contents 
-    // boths the key can be predefined or user-defined 
-    // Internally hashtable so the performance will depends upon the hash functions 
+    // value determine the associated contents and key uniquely identify the contents
+    // boths the key can be predefined or user-defined
+    // Internally hashtable so the performance will depends upon the hash functions
     // Internally it make the pair, so access m.first(key) and m.seconds(values)
 
 */
 
 
-#include<iostream>
-#include<unordered_map>
+// #include<iostream>
+// #include<unordered_map>
+// using namespace std;
+
+// int main() {
+
+//     // unordered_map<string, int> m;
+
+//     // // insert the key value like food item and price
+//     // m.insert(make_pair("maggi", 20));
+
+//     // m["noddles"] = 23;
+
+//     // // Iterate over the items 1. foreach and iterator
+//     // for(auto x : m) {
+//     //     cout << "key : " << x.first << " " << "value :" <<  x.second << endl;
+//     // }
+
+//     // // Iterate using the iterators
+//     // for(auto it = m.begin(); it != m.end(); it++) {
+//     //     cout << it->first  << " " << it->second << endl;
+//     // }
+
+
+
+
+//     return 0;
+// }
+
+
+// How to findout the occurrence of each elements in the arrays using the hash-table approaches
+#include<bits/stdc++.h>
 using namespace std;
 
 int main() {
 
-    unordered_map<string, int> m; 
+#ifndef ONLINE_JUDGE
+    freopen("../input.txt", "r", stdin);
+    freopen("../output.txt", "w", stdout);
+#endif
 
-    // insert the key value like food item and price 
-    m.insert(make_pair("maggi", 20));
+    unordered_map<int, int> map;
+    vector<int> v;
+    int n; cin >> n;
 
-    m["noddles"] = 23;
-
-    // Iterate over the items 1. foreach and iterator 
-    for(auto x : m) {
-        cout << "key : " << x.first << " " << "value :" <<  x.second << endl;
+    for (int i = 0; i < n; i++) {
+        int data; cin >> data;
+        v.push_back(data);
     }
 
-    // Iterate using the iterators 
-    for(auto it = m.begin(); it != m.end(); it++) {
-        cout << it->first  << " " << it->second << endl;
+
+    for (auto num : v) {
+        map[num]++;
     }
+
+    for (auto p : map) {
+        cout << p.first << " " << p.second << endl;
+    }
+
 
     return 0;
 }
