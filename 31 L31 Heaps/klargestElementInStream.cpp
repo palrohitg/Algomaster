b5573bae4369053a6fd3ca9bb545e003ab710b47
Vikas Pal <rohitprotobit@gmail.com>
2021-03-13 23:06:40 +0530

    min heap added

--- 31 L31 Heaps/klargestElementInStream.cpp
+++ 31 L31 Heaps/klargestElementInStream.cpp
@@ -3,3 +3,19 @@
 	1. we can't use the random access to priority_queue.
 
 */
+
+priority_queue<int, vector<int>, greater<int>> pq;
+int size;
+KthLargest(int k, vector<int> nums) {
+	size = k;
+	for (int i = 0; i < nums.size(); i++) {
+		pq.push(nums[i]);
+		if (pq.size() > k) pq.pop();
+	}
+}
+
+int add(int val) {
+	pq.push(val);
+	if (pq.size() > size) pq.pop();
+	return pq.top();
+}
