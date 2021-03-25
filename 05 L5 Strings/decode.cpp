c03b1f507fea695bb5bbed997fd18b5465758f7c
Vikas Pal <rohitprotobit@gmail.com>
2021-03-25 23:14:46 +0530

    page added

---
+++ 05 L5 Strings/decode.cpp
@@ -0,0 +1,13 @@
+// Deocode XORed arrays
+
+class Solution {
+public:
+	vector<int> decode(vector<int>& encoded, int first) {
+		for (int &n : encoded) {
+			swap(n, first);
+			first ^= n;
+		}
+		encoded.push_back(first);
+		return encoded;
+	}
+};
