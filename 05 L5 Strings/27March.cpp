bc7233b859e099dd87612f0d75d716b0e4f7649e
Vikas Pal <rohitprotobit@gmail.com>
2021-03-28 23:03:44 +0530

    new class added

--- 05 L5 Strings/27March.cpp
+++ 05 L5 Strings/27March.cpp
@@ -29,3 +29,76 @@ int balancedStringSplit(string s) {
 	return res;
 }
 
+
+// medium type questions
+// 5. longest palindromic strings DP
+/*
+	Approach :
+	1. let make a valid string by ignore all special character include only small character
+	2. Apply two pointer (start, end) to valid strings to check if is palindromic or not
+
+
+*/
+
+class Solution {
+public:
+	bool isPalindrome(string s) {
+
+		string valid;
+
+		for (auto ch : s) {
+			// insert this character
+			if ((ch >= 97 and ch <= 122)) {
+				valid.push_back(ch);
+			}
+			if (ch >= 65 and ch <= 90) {
+				valid.push_back(ch + 32);
+			}
+		}
+		cout << valid << endl;
+		// smaller letter
+		// apply two pointer to check
+		// palindromic or not
+		int end = valid.length();
+		int start = 0;
+
+
+		while (start < end) {
+			cout << valid[start] << " " << valid[end] << endl;
+			if (valid[start] == valid[end]) {
+				start++;
+				end--;
+			}
+
+			if (valid[start] != valid[end]) {
+				return false;
+			}
+		}
+
+		return true;
+	}
+};
+
+
+// new comments added
+
+class Solution {
+public:
+	bool isPalindrome(string s) {
+		if (s.empty()) return true;
+		int z = s.size();
+		int i = 0;
+		while (i < z) {
+			// cout << "comparing : " << s[i] << " and " << s[z] << endl;
+			if (!isalnum(s[z])) { z--; continue; }
+			if (!isalnum(s[i])) { i++; continue; }
+			if (tolower(s[i++]) != tolower(s[z--])) return false;
+		}
+		return true;
+	}
+};
+
+/*
+ * O(n) Time and
+ * O(1) Space
+ */
