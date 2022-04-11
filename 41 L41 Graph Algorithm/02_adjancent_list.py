9dd0c75c8136b90ab8d61b0dd2c6a14dbf8f8693
palrohitg <rohitprotobit@gmail.com>
2022-04-11 21:52:09 +0530

    adjlist adde

---
+++ 41 L41 Graph Algorithm/02_adjancent_list.py
@@ -0,0 +1,37 @@
+"""
+ Adjacent list implemenation of Graph
+
+
+# assingment of empty list here
+for i range(n):
+    self.adjList[i] = []
+
+for (src, dest, weight) in edges:
+            # allocate node in adjacency list from src to dest
+            self.adjList[src].append((dest, weight)) 
+"""
+class Graph:
+    #Constructors
+    def __init__(self, n, edges):
+        self.adjList = [[] for _ in range(n)]
+        print(self.adjList)
+        # add edges to the director graph
+        for (src, dest) in edges:
+            self.adjList[src].append(dest)
+
+    def printGraph(self):
+        # Graph print 
+        print(self.adjList)
+        for src in range(len(self.adjList)):
+            print(src, "==", end='')
+            # print current vetex and all its neighbouring vertices 
+            for dest in self.adjList[src]:
+               print(f"{dest}-->", end='')
+            print()
+
+if __name__ == "__main__":  
+    edges = [(0,1), (0,1), (0,1), (0,1), (0,1)]
+    n = 6 
+    graph = Graph(n, edges)
+    graph.printGraph()
+    pass
