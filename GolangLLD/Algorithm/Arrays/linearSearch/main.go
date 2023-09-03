779e4ba97ef9bb2485a7d1def1fcf39caabb5e52
Vikas Pal <vikas.pal@myparkplus.com>
2023-09-03 12:54:05 +0530

    day 1 coding things

--- GolangLLD/Algorithm/Arrays/linearSearch/main.go
+++ GolangLLD/Algorithm/Arrays/linearSearch/main.go
@@ -1,6 +1,9 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"sort"
+)
 
 func linearSearchAlgorithm(data []int, key int) bool {
 	for _, item := range data {
@@ -11,9 +14,36 @@ func linearSearchAlgorithm(data []int, key int) bool {
 	return false
 }
 
+// Find the Product subset whose subsums is less than a particular kth values
+
+func ProductCountSubarrayCount() int {
+	count := 0
+	/*
+
+		while(end < n) {
+			p
+			while ( start < end && k >= end) {
+				// drive the start
+				start++
+			}
+
+			if (p < k ) {
+				// calculate the answers
+			}
+		}
+
+
+	*/
+	return count
+}
+
 func main() {
-	dataList := []int{11, 22, 221}
-	key := 11
-	fmt.Println(linearSearchAlgorithm(dataList, key))
+	dataList := []int{109, 11, 22, 221} // slice where we don't have any specific numbers currently
+	// key := 11
+	// fmt.Println(linearSearchAlgorithm(dataList, key))
+
+	// Sorting of the slices
+	sort.Ints(dataList)
+	fmt.Println(dataList)
 	return
 }
